0a0df05dd712e72bd98cedb364d0ecb3c2185d69
flynnhillier <flynnhillier@gmail.com>
2025-02-25 18:56:25 +0000

    restructure auth schema to accomodate multiple oauth account connections

--- frontend/src/pages/Logout.page.tsx
+++ frontend/src/pages/Logout.page.tsx
@@ -19,10 +19,14 @@ export default function LogoutPage() {
   return (
     <>
 <<<<<<< HEAD
+<<<<<<< HEAD
       <p className="text-green-800"> Authenticated! - '{data?.user?.username ?? "-"}' </p>
 =======
       <p className="text-green-800"> Authenticated! - '{data?.user?.email ?? "-"}' </p>
 >>>>>>> 4407f90 (improved client side routing)
+=======
+      <p className="text-green-800"> Authenticated! - '{data?.user?.username ?? "-"}' </p>
+>>>>>>> 3905ba8 (restructure auth schema to accomodate multiple oauth account connections)
       <Button disabled={isPending} onClick={() => mutate(undefined)}>
         Logout
       </Button>
