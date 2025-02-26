ba10a16ca78f517a7474c3011a1ec34561fa1c85
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-02-26 17:24:14 +0000

    file declaratiom

--- frontend/src/components/recipe/RecipeSection.tsx
+++ frontend/src/components/recipe/RecipeSection.tsx
@@ -1,11 +1,16 @@
 export default function RecipeSection({ heading }: { heading: string}, { body }: { body: string }) {
   return(
     <>
+<<<<<<< HEAD
     <div className="recipeSection">
        <h2>{heading}:</h2>
       <p>{body}</p>
     </div>
      
+=======
+      <h2>{heading}:</h2>
+      <p>{body}</p>
+>>>>>>> 432e644 (file declaratiom)
     </>
   )
 }
