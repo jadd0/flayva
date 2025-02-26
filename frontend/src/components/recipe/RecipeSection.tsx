7b45a8b10dea532bf0119a7cff15eef8613a2c9f
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-02-26 17:27:24 +0000

    forgot i was working on main branch

--- frontend/src/components/recipe/RecipeSection.tsx
+++ frontend/src/components/recipe/RecipeSection.tsx
@@ -1,8 +1,11 @@
 export default function RecipeSection({ heading }: { heading: string}, { body }: { body: string }) {
   return(
     <>
-      <h2>{heading}:</h2>
+    <div className="recipeSection">
+       <h2>{heading}:</h2>
       <p>{body}</p>
+    </div>
+     
     </>
   )
 }
