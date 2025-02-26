907387659e7170375c5cf261c133735f22384b21
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-02-26 17:27:24 +0000

    forgot i was working on main branch

--- frontend/src/components/recipe/RecipeSection.tsx
+++ frontend/src/components/recipe/RecipeSection.tsx
@@ -2,6 +2,7 @@ export default function RecipeSection({ heading }: { heading: string}, { body }:
   return(
     <>
 <<<<<<< HEAD
+<<<<<<< HEAD
     <div className="recipeSection">
        <h2>{heading}:</h2>
       <p>{body}</p>
@@ -11,6 +12,13 @@ export default function RecipeSection({ heading }: { heading: string}, { body }:
       <h2>{heading}:</h2>
       <p>{body}</p>
 >>>>>>> 432e644 (file declaratiom)
+=======
+    <div className="recipeSection">
+       <h2>{heading}:</h2>
+      <p>{body}</p>
+    </div>
+     
+>>>>>>> 80d2b1c (forgot i was working on main branch)
     </>
   )
 }
