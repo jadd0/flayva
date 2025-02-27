609ae5d52ec0509a3cb75da8fb8b8290fa49b430
flynnhillier <flynnhillier@gmail.com>
2025-02-27 19:37:14 +0000

    api social router; user info by ID route

--- backend/src/server/routes/api/api.route.ts
+++ backend/src/server/routes/api/api.route.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import socialRouter from "@/server/routes/api/social.route";
+<<<<<<< HEAD
 import postRouter from "@server/routes/api/posts.route";
+=======
+>>>>>>> b48d77c (api social router; user info by ID route)
 
 const router: Router = Router();
 
 router.use("/s", socialRouter);
+<<<<<<< HEAD
 router.use("/p", postRouter);
+=======
+>>>>>>> b48d77c (api social router; user info by ID route)
 
 export default router;
--- backend/src/server/routes/api/social.route.ts
+++ backend/src/server/routes/api/social.route.ts
@@ -1,8 +1,14 @@
 import { db } from "@/db";
+<<<<<<< HEAD
 import { users as usersTable } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { Router, Request, Response } from "express";
 import { User } from "@flayva-monorepo/shared";
+=======
+import { users } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { Router, Request, Response } from "express";
+>>>>>>> b48d77c (api social router; user info by ID route)
 
 const router: Router = Router();
 
@@ -10,14 +16,22 @@ const router: Router = Router();
  * Get a user's profile
  */
 router.get("/u/:userID", async (req: Request, res: Response) => {
+<<<<<<< HEAD
   const users = await db.select().from(usersTable).where(eq(usersTable.id, req.params.userID));
   const user = users[0];
 
   if (!user)
+=======
+  const profiles = await db.select().from(users).where(eq(users.id, req.params.userID));
+  const profile = profiles[0];
+
+  if (!profile)
+>>>>>>> b48d77c (api social router; user info by ID route)
     res.status(404).send({
       exists: false,
       message: "Profile not found",
     });
+<<<<<<< HEAD
 
   const structuredUser: User = {
     bio: user.bio,
@@ -30,6 +44,18 @@ router.get("/u/:userID", async (req: Request, res: Response) => {
     exists: true,
     user: structuredUser,
   });
+=======
+  else
+    res.status(200).send({
+      exists: true,
+      profile: {
+        id: profile.id,
+        username: profile.username,
+        bio: profile.bio,
+        profile_picture_url: profile.profile_picture_url ?? undefined,
+      },
+    });
+>>>>>>> b48d77c (api social router; user info by ID route)
 });
 
 export default router;
